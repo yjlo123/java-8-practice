fded684de793b4c97c4a615ead96b7040c0b845a
Siwei <liusiwei.yjlo@gmail.com>
2016-10-13 12:35:35 +0800

    Square brackets in Lambda

--- Lambdas/Lambda.java
+++ Lambdas/Lambda.java
@@ -3,5 +3,10 @@ import java.util.*;
 class Lambda{
 	public static void main(String[] args){
 		Arrays.asList( "a", "b", "c" ).forEach( e -> System.out.println( e ) );
+		
+		String separator = ",";
+		Arrays.asList( "a", "b", "c" ).forEach( e -> {
+			System.out.print( e + separator );
+		});
 	}
 }
